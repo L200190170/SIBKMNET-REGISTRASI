544f9e0eaa3253e1425cfec8f80d00ad0eeee95f
Selvianisa Cahya Mukti <55840976+L200190170@users.noreply.github.com>
2022-10-24 09:11:36 +0700

    Registrasi

---
+++ SIBKMNET_WebApp/ViewModels/ChangePassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApp.ViewModels
+{
+    public class ChangePassword
+    {
+        [Key]
+        public string CurrentPass { get; set; }
+        public string NewPass { get; set; }
+        [Compare("NewPass", ErrorMessage = "Konfirmasi kata sandi baru tidak cocok")]
+        public string ConfirmPass { get; set; }
+    }
+}
